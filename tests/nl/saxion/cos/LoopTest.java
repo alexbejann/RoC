4711b2ef61136b729c27a6adcc6cc2efd6aa3310
alexbejann <474404@student.saxion.nl>
2021-04-02 19:15:42 +0300

    Implemented do while loop and added comments and more tests

--- tests/nl/saxion/cos/LoopTest.java
+++ tests/nl/saxion/cos/LoopTest.java
@@ -15,7 +15,7 @@ public class LoopTest extends TestBase
      * Test while loop
      */
     @Test
-    public void forLoopWithIfStatement() throws IOException, AssembleException
+    public void whileLoop() throws IOException, AssembleException
     {
         String codeString = "functia main()\n" +
                             "{\n" +
@@ -36,6 +36,68 @@ public class LoopTest extends TestBase
 
         assertArrayEquals(new String[] {
                 "10"
+        }, output.toArray());
+
+    }
+
+    /**
+     * Test do while loop
+     */
+    @Test
+    public void doWhileLoop() throws IOException, AssembleException
+    {
+        String codeString = "functia main()\n" +
+                "{\n" +
+                "   numar a<-2*3\n" +
+                "   a<-a+1\n" +
+                "   executa {\n" +
+                "       a<-a+1\n" +
+                "   }cat timp(a < 10)\n" +
+                "   printeaza(a)\n" +
+                "}";
+
+        Compiler c = new Compiler();
+        JasminBytecode code = c.compileString(codeString,"doWhileLoop");
+        Assertions.assertNotNull(code);
+
+        List<String> output = runCode(code);
+
+        assertArrayEquals(new String[] {
+                "10"
+        }, output.toArray());
+
+    }
+
+    /**
+     * Test do while and while together
+     */
+    @Test
+    public void doWhileAndWhileTogetherLoop() throws IOException, AssembleException
+    {
+        String codeString = "functia main()\n" +
+                "{\n" +
+                "   numar a<-2*3\n" +
+                "   a<-a+1\n" +
+                "   executa {\n" +
+                "       a<-a+1\n" +
+                "   }cat timp(a < 10)\n" +
+                "   numar b<-5\n" +
+                "   cat timp(b < 20) executa\n" +
+                "   {\n" +
+                "       b<-b+1\n" +
+                "   }\n" +
+                "   printeaza(a)\n" +
+                "   printeaza(b)\n" +
+                "}";
+
+        Compiler c = new Compiler();
+        JasminBytecode code = c.compileString(codeString,"doWhileLoop");
+        Assertions.assertNotNull(code);
+
+        List<String> output = runCode(code);
+
+        assertArrayEquals(new String[] {
+                "10","20"
         }, output.toArray());
 
     }
