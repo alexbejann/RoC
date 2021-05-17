7017a15a1dcbb8082e5ddaabb92fa9ad2f448a6e
alexbejann <474404@student.saxion.nl>
2021-05-17 21:23:51 +0300

    added ExpressionsTest.java

---
+++ tests/nl/saxion/cos/ExpressionsTest.java
@@ -0,0 +1,36 @@
+package nl.saxion.cos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ExpressionsTest extends TestBase
+{
+
+    /*
+     * Expected same output to be the same
+     */
+    @Test
+    public void expressionTest() throws IOException, AssembleException
+    {
+        String codeString = "functia main()\n" +
+                            "{\n" +
+                            "  " +
+                            "}";
+
+        Compiler c = new Compiler();
+        JasminBytecode code = c.compileString(codeString,"expressionTest");
+        Assertions.assertNotNull(code);
+
+        List<String> output = runCode(code);
+
+        assertArrayEquals(new String[] {
+                ""
+        }, output.toArray());
+
+    }
+}
